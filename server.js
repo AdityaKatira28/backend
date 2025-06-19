86daba4b5a9bd3bd59991e9abf346865db7c89a5
AdityaKatira28 <adityakatira@hotmail.com>
2025-06-19 09:46:51 +0530

    commit

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-// Allow requests only from your Netlify frontend
-app.use(
-  cors({
-    origin: "https://frontenduidashboard.netlify.app", 
-    credentials: true, // If using cookies/sessions
-  })
-);
+// CORS configuration - ADD THIS CODE
+app.use(cors({
+  origin: ['https://frontenduidashboard.netlify.app', 'http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
+
+// Rest of your server code continues here...
 
 // Test route
 app.get("/api/test", (req, res) => {
@@ -23,11 +24,3 @@ app.use(require("./routes/test"));
 
 
 
-// CORS configuration - ADD THIS CODE
-app.use(cors({
-  origin: ['https://frontenduidashboard.netlify.app', 'http://localhost:3000'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true
-}));
-
-// Rest of your server code continues here...
