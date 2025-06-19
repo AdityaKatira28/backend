fbdec696ce94f2d8206686b1fc181ff129068509
AdityaKatira28 <adityakatira@hotmail.com>
2025-06-19 09:23:20 +0530

    commit

--- server.js
+++ server.js
@@ -20,3 +20,14 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.use(require("./routes/test"));
+
+
+
+// CORS configuration - ADD THIS CODE
+app.use(cors({
+  origin: ['https://frontenduidashboard.netlify.app', 'http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
+
+// Rest of your server code continues here...
